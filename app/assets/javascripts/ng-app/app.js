d91915a1e02f2c1e953d6cd31f879ab12e3ad827
Chintan Desai <chintan.desai@botreeconsulting.com>
2015-03-11 17:12:07 +0530

    my first commit

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,94 @@
+function homeController($scope,$http){
+
+
+	// Value 1 is Other regular costing cost and value 2 is the My costing
+	$http.get('/assets/ng-app/connection.properties').then(function (response) {
+		$scope.questions = response.data
+		$scope.selected_entries_radio={}
+		$scope.selected_entries_checkbox={}
+		$scope.total_my = 0
+		$scope.person_days_my = 0
+		$scope.personnel_my = 0
+		$scope.total_actual = 0
+		$scope.person_days_actual = 0
+		$scope.personnel_actual = 0
+		angular.forEach($scope.questions, function(question) {
+			id = question.id
+			if (question.type=="checkbox"){
+				$scope.selected_entries_checkbox[id] = {}
+				angular.forEach(question.options, function(option) {
+					opt_id = option.id
+					$scope.selected_entries_checkbox[id][opt_id] = null
+				})
+			}else{
+				$scope.selected_entries_radio[id] = null
+			}
+		})
+	})
+
+	// Initialize variables
+
+	// Function which calculates the score after selecting / checking the option
+	$scope.calculateScore = function(question,option){
+		// Set value in appropriate variable
+		if (question.type=="checkbox"){
+			// Calculation for check box
+			angular.forEach(document.getElementsByName("question_"+question.id), function(ele) {
+				if (ele.id == "option_"+option.id){
+					if(ele.checked){
+						//	When the check box element is checked
+						$scope.selected_entries_checkbox[question.id][option.id] = option
+					}else{
+						//	When the check box element is unchecked
+						$scope.selected_entries_checkbox[question.id][option.id] = null
+					}
+				}
+			})
+		}else{
+			// Calculation for radio button
+			$scope.selected_entries_radio[question.id] = option
+		}
+
+		// Set all counters to zero and calculate totals
+		$scope.total_my = 0
+		$scope.person_days_my = 0
+		$scope.personnel_my = 0
+		$scope.total_actual = 0
+		$scope.person_days_actual = 0
+		$scope.personnel_actual = 0
+		// Add total of all radio inputs
+		angular.forEach($scope.selected_entries_radio, function(value, key) {
+			if (value!=null) {
+				addToTotalScoreboard(value)
+			}
+		})
+		// Add total of all check box inputs
+		angular.forEach($scope.selected_entries_checkbox, function(value, key) {
+			angular.forEach(value, function(v, k) {
+				if (v!=null) {
+					addToTotalScoreboard(v)
+				}
+			})
+		})
+	}
+
+	function addToTotalScoreboard(value){
+		$scope.total_my = $scope.total_my + value["value2"]
+		$scope.total_actual = $scope.total_actual + value["value1"]
+
+		$scope.person_days_my = $scope.person_days_my + value["person_days2"]
+		$scope.person_days_actual = $scope.person_days_actual + value["person_days1"]
+
+		$scope.personnel_my = $scope.personnel_my + value["personnel2"]
+		$scope.personnel_actual = $scope.personnel_actual + value["personnel1"]
+	}
+
+}
+
+//	if ($scope.personnel_my==0){
+//		$scope.personnel_my = $scope.personnel_my + v["first_selection_personnel2"]
+//		$scope.personnel_actual = $scope.personnel_actual + v["first_selection_personnel1"]
+//	}else{
+//		$scope.personnel_my = $scope.personnel_my + v["personnel2"]
+//		$scope.personnel_actual = $scope.personnel_actual + v["personnel1"]
+//	}
